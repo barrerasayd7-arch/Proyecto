7b85d2f6b53079ea514e533c2f7097edc1e04f4f
Sayd Barrera <barrerasayd7@gmail.com>
2026-04-26 13:06:34 -0500

    se añadieron esas funciones al solicitud.routers

--- backend/src/routes/solicitud.routes.js
+++ backend/src/routes/solicitud.routes.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { crearSolicitud } from "../controllers/solicitud.controller.js";
+import {
+  crearSolicitud,
+  getMisSolicitudes,
+  getSolicitudesRecibidas,
+  responderSolicitud,
+} from "../controllers/solicitud.controller.js";
 
 const router = Router();
 
-router.post("/", crearSolicitud);
+router.post("/",                    crearSolicitud);
+router.get("/enviadas/:id",         getMisSolicitudes);
+router.get("/recibidas/:id",        getSolicitudesRecibidas);
+router.post("/responder",           responderSolicitud);
 
 export default router;
